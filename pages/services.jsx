bbaeafb96451c2c54c9e61383fe8a3695ec2f864
Philani Ncube <ncbphi001@gmail.com>
2022-04-25 22:43:14 +0200

    price

--- pages/services.jsx
+++ pages/services.jsx
@@ -40,7 +40,7 @@ const services = ({ products }) => {
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">
-                    {product.price}
+                    R {product.price.toFixed(2)}
                   </p>
                 </div>
               </Link>
